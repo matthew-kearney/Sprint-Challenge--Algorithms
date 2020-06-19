d609c9bf60ec69b35264c7f3df45f757c660bf52
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-06-19 10:46:05 -0500

    count_th.py done

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,20 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
     
-    pass
+    key = "th"
+    var1 = len(word)
+    var2 = len(key)
+    # Base Case
+    if (var1 == 0 or var1 < var2):
+        return 0
+    # Recursive Case
+    # Checking if the first substring matches
+    if (word[0: var2] == key):
+        return count_th(word[var2 - 1:]) + 1
+
+    # Otherwise, return the count
+    # from the remaining index
+    # repeats
+    return count_th(word[var2 - 1:])
+# no loops
