35bf15a62dc664f0b4a9202937635d0d24a04946
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-06-19 11:34:51 -0500

    added answers

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -10,15 +10,17 @@ def count_th(word):
     var1 = len(word)
     var2 = len(key)
     # Base Case
+    # if the word equals 0 or the word is less then the key return 0
     if (var1 == 0 or var1 < var2):
         return 0
     # Recursive Case
     # Checking if the first substring matches
+    # if it does then return it
     if (word[0: var2] == key):
         return count_th(word[var2 - 1:]) + 1
 
-    # Otherwise, return the count
-    # from the remaining index
+    # If not, return the count
+    # from remaining index
     # repeats
     return count_th(word[var2 - 1:])
 # no loops
